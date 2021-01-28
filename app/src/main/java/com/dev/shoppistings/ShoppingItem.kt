5da8da13b00edc8f3a50cd51ca378e42351353a1
Adhip_Kushwaha <adhipkushwaha301199@gmail.com>
2021-01-28 21:32:49 +0530

    Entity for DB (ShoppingItems)

---
+++ app/src/main/java/com/dev/shoppistings/ShoppingItem.kt
@@ -0,0 +1,17 @@
+package com.dev.shoppistings
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_items")
+data class ShoppingItem (
+    @ColumnInfo(name = "item_name")
+    var name:String,
+    @ColumnInfo(name = "item_amount")
+    var amount:Int
+        )
+{
+    @PrimaryKey(autoGenerate = true)
+        var id:Int? = null
+}
